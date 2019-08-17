09d465852c3cfde0ca105cd4fd68c6c6193ef344
vissarion tsitoglou <vissarionas@gmail.com>
2019-08-17 19:49:20 +0200

    Add redux and implement basic state management functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = ({ title }) => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>{title}</p>
+    </header>
+  </div>
+);
 
-export default App;
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  title: state.title,
+});
+
+export default connect(mapStateToProps)(App);
