e1069c29cc8c8956883a7fbfed61a8432cff9a0c
Vissarion Tsitoglou <vissarion.tsitoglou@ibm.com>
2019-08-22 15:14:19 +0200

    Modify eslint script paths

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,5 +4,5 @@ import reducer from './reducer';
 
 export default createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
