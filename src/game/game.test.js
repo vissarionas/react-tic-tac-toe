ff2afbd19a1e581006394eb4ab370f8846236f03
Vissarion Tsitoglou <vissarionas@gmail.com>
2019-08-23 14:22:11 +0200

    Remove depricated App test

--- src/game/game.test.js
+++ src/game/game.test.js
@@ -1,11 +1,21 @@
 /* eslint-disable no-undef */
-import testWrapper from '../redux/testWrapper';
+import { fireEvent, cleanup } from '@testing-library/react';
+import renderWithRedux from '../redux/renderWithRedux';
+import '@testing-library/jest-dom/extend-expect';
 import Game from './game';
 
 describe('Game', () => {
+  afterEach(cleanup);
+
   it('renders 9 tiles', () => {
-    const { getAllByTestId } = testWrapper(Game);
+    const { getAllByTestId } = renderWithRedux(Game);
     const gameTiles = getAllByTestId('game-tile');
     expect(gameTiles).toHaveLength(9);
+  });
+  it('updates the game', () => {
+    const { getAllByTestId, getByText } = renderWithRedux(Game);
+    const gameTiles = getAllByTestId('game-tile');
+    fireEvent.click(gameTiles[0]);
+    expect(getByText('user')).toBeInTheDocument();
   });
 });
